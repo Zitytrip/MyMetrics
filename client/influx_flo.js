3ca7a39d3458de446670d9631b5081e58863a20a
root <root@c341caeb0e01>
2018-01-17 23:37:54 +0000

    influx query demo added

---
+++ client/influx_flo.js
@@ -0,0 +1,29 @@
+const Influx = require ("influx")
+const os = require('os');
+
+
+async function demo () {
+    const influx = new Influx.InfluxDB({
+        host: 'localhost',
+        database: 'demo'
+    });
+
+    var dbnames = await influx.getDatabaseNames();
+    console.log(dbnames);
+
+    var query = `
+        select * from zitytrip3`;
+
+//        where host = ${Influx.escape.stringLit(os.hostname())}
+//        order by time desc
+//        limit 10
+//    `;
+
+    var result = await influx.query(query);
+    console.log(result);
+    result.map (r => {
+        console.log(r);
+    });
+
+}
+demo();
